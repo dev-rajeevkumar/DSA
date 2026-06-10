d466a2a541ba3761289992cc3e51301032c2d832
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-10 14:58:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.4%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.size()==0)
-            return true;
-        int j=0;
-        string temp="";
-        for(int i=0;i<s.size();i++){
-            for(j;j<t.size();j++){
-                if(s[i]==t[j]){
-                    temp.push_back(t[j]);
-                    j++;
-                    break;
-                }
-                    
+        int a=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]==s[a]){
+                a++;
             }
         }
-        if(s==temp)
+        if(a==s.size())
             return true;
         return false;
+        // if(s.size()==0)
+        //     return true;
+        // int j=0;
+        // string temp="";
+        // for(int i=0;i<s.size();i++){
+        //     for(j;j<t.size();j++){
+        //         if(s[i]==t[j]){
+        //             temp.push_back(t[j]);
+        //             j++;
+        //             break;
+        //         }
+                    
+        //     }
+        // }
+        // if(s==temp)
+        //     return true;
+        // return false;
     }
 };
