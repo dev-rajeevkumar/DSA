133df5cf0f493f991fe6dedcf8e12924fe8de322
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-13 00:27:39 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,16 +3,27 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())
             return false;
+        vector<int> freqs(26,0);
+        vector<int> freqt(26,0);
         for(int i=0;i<s.size();i++){
-            for(int j=0;j<t.size();j++){
-                if(s[i]==t[j]){
-                    t.erase(j, 1);
-                    break;
-                }
-            }
+            freqs[s[i]-'a']++;
         }
-        if(t.size()==0)
+        for(int i=0;i<t.size();i++){
+            freqt[t[i]-'a']++;
+        }
+        if(freqs==freqt)
             return true;
         return false;
+        // for(int i=0;i<s.size();i++){
+        //     for(int j=0;j<t.size();j++){
+        //         if(s[i]==t[j]){
+        //             t.erase(j, 1);
+        //             break;
+        //         }
+        //     }
+        // }
+        // if(t.size()==0)
+        //     return true;
+        // return false;
     }
 };
