1297db9963a936f316e81b38e7c072f61372dc0b
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-13 22:37:06 +0530

    Time: 0 ms (100%), Space: 9.5 MB (85.81%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> ans=board;
         int n=board.size();
         int m=board[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int c=0;
-                if(j-1>=0 && board[i][j-1]==1)
+                if(j-1>=0 && board[i][j-1]%10==1)
                     c++;
-                if(j+1<m && board[i][j+1]==1)
+                if(j+1<m && board[i][j+1]%10==1)
                     c++;
-                if(i-1>=0 && j-1>=0 && board[i-1][j-1]==1)
+                if(i-1>=0 && j-1>=0 && board[i-1][j-1]%10==1)
                     c++;
-                if(i-1>=0 && board[i-1][j]==1)
+                if(i-1>=0 && board[i-1][j]%10==1)
                     c++;
-                if(i-1>=0 && j+1<m && board[i-1][j+1]==1)
+                if(i-1>=0 && j+1<m && board[i-1][j+1]%10==1)
                     c++;
-                if(i+1<n && j-1>=0 && board[i+1][j-1]==1)
+                if(i+1<n && j-1>=0 && board[i+1][j-1]%10==1)
                     c++;
-                if(i+1<n && board[i+1][j]==1)
+                if(i+1<n && board[i+1][j]%10==1)
                     c++;
-                if(i+1<n && j+1<m && board[i+1][j+1]==1)
+                if(i+1<n && j+1<m && board[i+1][j+1]%10==1)
                     c++;
-                if((board[i][j]==1 && (c==2||c==3))||(board[i][j]==0 && c==3))
-                    ans[i][j]=1;
-                else
-                    ans[i][j]=0;
+                if((board[i][j]%10==1 && (c==2||c==3))||(board[i][j]%10==0 && c==3))
+                    board[i][j]+=10;
             }
         }
-        board=ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]/=10;
+            }
+        }
     }
 };
