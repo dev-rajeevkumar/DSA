04c4dcad1eed83eaa38927ab73a0982b1759a60d
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-14 14:06:51 +0530

    Time: 0 ms (100%), Space: 8.7 MB (67.38%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i=1;
-        while(n-i>=0){
-            n=n-i;
-            i++;
-        }
-        return i-1;
+        return (sqrt(8*(long long)n+1)-1)/2;
+        // int i=1;
+        // while(n-i>=0){
+        //     n=n-i;
+        //     i++;
+        // }
+        // return i-1;
     }
 };
