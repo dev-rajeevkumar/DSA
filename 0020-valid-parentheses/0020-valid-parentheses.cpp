ccf29806c64ae352bdee83442cbdb5b6cf0b0d20
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-17 23:27:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (88.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        string t="";
+        for(char x:s){
+            if(x=='(' || x=='{' || x=='['){
+                t.push_back(x);
+                continue;
+            }
+            if(x==')' && t.size()>0 && t[t.size()-1]=='('){
+                t.pop_back();
+                continue;
+            }
+            if(x==']' && t.size()>0 && t[t.size()-1]=='['){
+                t.pop_back();
+                continue;
+            }
+            if(x=='}' && t.size()>0 && t[t.size()-1]=='{'){
+                t.pop_back();
+                continue;
+            }
+            return false;
+        }
+        if(t.size()>0) return false;
+        return true;
+    }
+};
