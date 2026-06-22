5b1bb40d2ad325fe9d3ed390b4efdaaa2d9904cc
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-23 00:28:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.63%) - LeetHub

--- 0793-preimage-size-of-factorial-zeroes-function/0793-preimage-size-of-factorial-zeroes-function.cpp
+++ 0793-preimage-size-of-factorial-zeroes-function/0793-preimage-size-of-factorial-zeroes-function.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     long long f(long long n) {
-        long long a=(n/5)+(n/25)+(n/125)+(n/625)+(n/3125)+(n/15625)+(n/78125)+(n/390625)+(n/1953125)+(n/9765625)+(n/48828125)+(n/244140625)+(n/1220703125);
-        return a;
+        return (n/5)+(n/25)+(n/125)+(n/625)+(n/3125)+(n/15625)+(n/78125)+(n/390625)+(n/1953125)+(n/9765625)+(n/48828125)+(n/244140625)+(n/1220703125);
     }
     int preimageSizeFZF(int k) {
-        long n=0,x=k;
-        n=n+(x*4);
-        // if(k>510000000) return 5;
+        long x=k;
+        long n=(x*4);
         while(f(n)<=k){
             if(f(n)==k){
                 return 5;
