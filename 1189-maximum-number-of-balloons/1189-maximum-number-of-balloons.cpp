62ec5bd024378118aa81f70f7bf8f7937d54c8d5
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-22 07:31:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (95.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -9,12 +9,6 @@ public:
             else if(c=='o')f[3]++;
             else if(c=='n')f[4]++;
         }
-        f[2]=f[2]/2;
-        f[3]=f[3]/2;
-        int mini=INT_MAX;
-        for(int n:f){
-            if(n<mini) mini=n;
-        }
-        return mini;
+        return min({f[0],f[1],f[2]/2,f[3]/2,f[4]});
     }
 };
