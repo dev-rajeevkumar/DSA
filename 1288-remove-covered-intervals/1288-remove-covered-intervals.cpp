e78c79f050591fffe49b9193a94349640fcf093a
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-07-06 06:48:21 +0530

    Time: 55 ms (9.8%), Space: 15.3 MB (30.6%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,9 +3,15 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
     sort(intervals.begin(),intervals.end());
     for(int i=0;i<intervals.size();i++){
-        for(int j=0;j<intervals.size();j++){
-            if(i==j)continue;
-            if(intervals[j][0]<=intervals[i][0] && intervals[i][1]<=intervals[j][1]){
+        if(i-1>=0){
+            if(intervals[i-1][0]<=intervals[i][0] && intervals[i][1]<=intervals[i-1][1]){
+                intervals.erase(intervals.begin()+i);
+                i--;
+                continue;
+            }
+        }
+        if(i+1<intervals.size()){
+            if(intervals[i+1][0]<=intervals[i][0] && intervals[i][1]<=intervals[i+1][1]){
                 intervals.erase(intervals.begin()+i);
                 i--;
                 continue;
