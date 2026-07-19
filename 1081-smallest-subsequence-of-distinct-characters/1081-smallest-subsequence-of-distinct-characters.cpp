1e8bde5cf60761c57787ee72ae156bad2f740ecb
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-07-19 23:06:29 +0530

    Time: 2 ms (17.88%), Space: 9.5 MB (5.59%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool check(string &s, string &u, int n){
+    vector<int> f(26);
+    for(int i=n;i<s.size();i++)
+        f[s[i]-'a']++;
+
+    for(char c : u){
+        if(f[c-'a']==0)
+            return false;
+    }
+    return true;
+    }
+    string smallestSubsequence(string s) {
+        string u;
+        vector<int> f(26);
+        string ans="";
+        for(char c:s)f[c-'a']++;
+        for(int i=0;i<26;i++)
+            if(f[i])u+=char(i+'a');
+        while(u.size()>0){
+            for(int i=0;i<u.size();i++){
+                int flag=0;
+                for(int j=0;j<s.size();j++){
+                    if(s[j]==u[i] && check(s,u,j)){
+                        ans.push_back(u[i]);
+                        u.erase(i,1);
+                        s=s.substr(j+1,s.size()-j);
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==1)break;
+            }
+        }
+        return ans;
+    }
+};
