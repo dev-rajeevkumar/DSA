2b2f0af5ffc3b541a9bcd3df087819134945935b
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-09-02 21:31:35 +0530

    Time: 77 ms (37.89%), Space: 297.3 MB (32.81%) - LeetHub

--- 3809-best-reachable-tower/3809-best-reachable-tower.cpp
+++ 3809-best-reachable-tower/3809-best-reachable-tower.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
         vector<vector<int>> ans;
-        sort(towers.begin(),towers.end());
         for(int i=0;i<towers.size();i++){
             int d=abs(towers[i][0]-center[0])+abs(towers[i][1]-center[1]);
-            if(d<=radius)
-                ans.push_back({towers[i][2],towers[i][0],towers[i][1]});
-        }
-        if(ans.size()==0)return {-1,-1};
-        sort(ans.rbegin(),ans.rend());
-        int qf=ans[0][0];
-        vector<vector<int>> ans2;
-        for(int i=0;i<ans.size();i++){
-            if(ans[i][0]==qf){
-                ans2.push_back({ans[i][1],ans[i][2]});
+            if(d<=radius){
+                if(ans.empty()){
+                    ans.push_back({towers[i][2],towers[i][0],towers[i][1]});
+                }
+                else if(towers[i][2]>ans[0][0]){
+                    ans.clear();
+                    ans.push_back({towers[i][2],towers[i][0],towers[i][1]});
+                }
+                else if(towers[i][2]==ans[0][0]){
+                    ans.push_back({towers[i][2],towers[i][0],towers[i][1]});
+                }
             }
         }
-        sort(ans2.begin(),ans2.end());
-        return ans2[0];
+        if(ans.size()==0)return {-1,-1};
+        sort(ans.begin(),ans.end());
+        return {ans[0][1],ans[0][2]};
     }
 };
