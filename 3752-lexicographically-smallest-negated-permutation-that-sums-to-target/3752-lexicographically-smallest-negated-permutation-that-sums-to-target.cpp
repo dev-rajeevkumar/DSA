c14a2de80aac4413b6811387400d095bc6381b5d
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-09-06 14:55:33 +0530

    Time: 35 ms (68.45%), Space: 126.3 MB (8.02%) - LeetHub

--- 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> lexSmallestNegatedPerm(int n, long long target) {
-         vector<int> ans;
+        vector<int> ans;
         long long sum=0;
         for(int i=1;i<=n;i++){
             sum+=i;
@@ -14,8 +14,16 @@ public:
             }
             if(sum==target)break;
         }
-        sort(ans.begin(),ans.end());
         if(sum!=target)return {};
-        return ans;
+        vector<int> res;
+        for(int i=n-1;i>=0;i--){
+            if(ans[i]<0)
+                res.push_back(ans[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(ans[i]>0)
+                res.push_back(ans[i]);
+        }
+        return res;
     }
 };
