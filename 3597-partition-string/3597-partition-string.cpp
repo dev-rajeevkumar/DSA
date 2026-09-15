386ac47c441baacc2f50c540746c3aa72a74063d
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-09-15 20:05:52 +0530

    Time: 746 ms (14.55%), Space: 114.5 MB (47.39%) - LeetHub

---
+++ 3597-partition-string/3597-partition-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> partitionString(string s) {
+        vector<string> ans;
+        set<string> t;
+        string x="";
+        for(int i=0;i<s.size();i++){
+            x.push_back(s[i]);
+            if(!t.count(x)){
+                ans.push_back(x);
+                t.insert(x);
+                x="";
+            }
+        }
+        return ans;
+    }
+};
