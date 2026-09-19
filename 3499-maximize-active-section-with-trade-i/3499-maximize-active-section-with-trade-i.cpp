dfde64c649bfedb1f4da6f2ee13b75b0937d500b
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-09-19 12:16:15 +0530

    Time: 84 ms (64.4%), Space: 54.8 MB (86.4%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        vector<int> zero;
-        int c=0;
-        int one=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0')c++;
-            else{
-                one++;
-                if(c>0)zero.push_back(c);
-                c=0;
+        int c=0,one=0,maxi=0,a=0,b=0,i=0;
+        for(char x:s)if(x=='1')one++;
+        for(i;i<s.size();i++){
+            if(s[i]=='0')a++;
+            else if(a>0&&s[i]=='1'){
+                c++;
+                break;
             }
         }
-        if(c>0)zero.push_back(c);
-        if(zero.size()==0)return s.size();
-        if(zero.size()==1)return s.size()-zero[0];
-        int maxi=0;
-        for(int i=0;i<zero.size()-1;i++){
-            maxi=max(maxi,zero[i]+zero[i+1]);
+        if(c==0&&a>0)c=1;
+        for(i;i<s.size();i++){
+            if(s[i]=='0')b++;
+            else{
+                if(b>0){
+                    maxi=max(maxi,a+b);
+                    c++;
+                    a=b;
+                }
+                b=0;
+            }
         }
+        if(b>0){
+            maxi=max(maxi,a+b);
+            c++;
+        }
+        if(c==0)return s.size();
+        if(c==1)return s.size()-a;
         return one+maxi;
     }
 };
